0e724ebb0932b106b1883c91b5da2f678442950a
Akshay Rangasai <akshayrangasai.d@gmail.com>
2012-07-02 16:41:23 +0530

    Added JS

---
+++ README.md~
@@ -0,0 +1,4 @@
+explodingcircles
+================
+
+Circles Explode to make other circles. Kind of a navigation thing 
---
+++ circles.js
@@ -0,0 +1,20 @@
+var radius = {} || radius;
+radius.inner = "100px";
+radius.middle = "200px";
+radius.outer = "250px";
+var elems = {} || elems;
+elems.inner = 10;
+elems.middle = 10;
+elems.outer = 10;
+//Total Width/Height = 500px; Thus we can fir these circles in a 510 by 510 box :D
+var layers = {} || layers;
+					
+function createcirclelocs(elem,rad)
+	{
+	var elements = new Array;
+	var i = 0;
+	while(i<elem)
+		{
+		
+		}
+	}
